d84986d6c5c6922203442349c82164a764b27978
luvellana <luciananunezl99@gmail.com>
2021-12-15 18:52:41 -0400

    arreglado los componentes, declarados en app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { FiltrosComponent } from './components/filtros/filtros.component';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
+import { DestacadosComponent } from './components/destacados/destacados.component';
+import { FavoritosComponent } from './components/favoritos/favoritos.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, FavoritosComponent, DestacadosComponent, ConfiguracionComponent, HomeComponent, LoginComponent, SignupComponent, FiltrosComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
