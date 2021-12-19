e3eda1d79860e2ff5d1197638111c06833e3f463
H0OE <rodrigoquiroz308@gmail.com>
2021-12-19 07:27:47 -0400

    2 files modified

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor( ) {}
+  model = {
+    user: '',
+    pass: '',
+  };
+
+  constructor() {}
 
   ngOnInit() {}
+
+  onSubmit() {
+    console.log(this.model);
+  }
 }
