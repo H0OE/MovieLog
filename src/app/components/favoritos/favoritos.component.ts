f57636566d093ebacbaa5ad9758d427f70a3d57f
luvellana <luciananunezl99@gmail.com>
2021-12-19 02:00:20 -0400

    xd

--- src/app/components/favoritos/favoritos.component.ts
+++ src/app/components/favoritos/favoritos.component.ts
@@ -6,7 +6,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favoritos.component.css']
 })
 export class FavoritosComponent implements OnInit {
+  public peliculas = [
+    {
+      titulo: 'The Simpsons Movie',
+      descripcion: '',
+      portada:'https://flxt.tmsimg.com/assets/p166206_p_v13_ao.jpg',
+      portada2: 'https://superfanatico.files.wordpress.com/2012/05/2266997.jpg',
+      trailer: ''
 
+    },
+    {
+      titulo: 'The Simpsons Movie',
+      portada:'https://flxt.tmsimg.com/assets/p166206_p_v13_ao.jpg'
+
+    },
+    {
+      titulo: 'The Simpsons Movie',
+      portada:'https://flxt.tmsimg.com/assets/p166206_p_v13_ao.jpg'
+
+    },
+    {
+      titulo: 'The Simpsons Movie',
+      portada:'https://flxt.tmsimg.com/assets/p166206_p_v13_ao.jpg'
+
+    },
+    {
+      titulo: 'The Simpsons Movie',
+      portada:'https://flxt.tmsimg.com/assets/p166206_p_v13_ao.jpg'
+
+    },
+    {
+      titulo: 'The Simpsons Movie',
+      portada:'https://flxt.tmsimg.com/assets/p166206_p_v13_ao.jpg'
+
+    },
+    {
+      titulo: 'The Simpsons Movie',
+      portada:'https://flxt.tmsimg.com/assets/p166206_p_v13_ao.jpg'
+
+    },
+    {
+      titulo: 'The Simpsons Movie',
+      portada:'https://flxt.tmsimg.com/assets/p166206_p_v13_ao.jpg'
+
+    },
+    {
+      titulo: 'The Simpsons Movie',
+      portada:'https://flxt.tmsimg.com/assets/p166206_p_v13_ao.jpg'
+
+    },
+    {
+      titulo: 'The Simpsons Movie',
+      portada:'https://flxt.tmsimg.com/assets/p166206_p_v13_ao.jpg'
+
+    },
+    {
+      titulo: 'The Simpsons Movie',
+      portada:'https://flxt.tmsimg.com/assets/p166206_p_v13_ao.jpg'
+
+    },
+    {
+      titulo: 'The Simpsons Movie',
+      portada:'https://flxt.tmsimg.com/assets/p166206_p_v13_ao.jpg'
+
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
